5f74606261d970b69a495d0c3cd664b5a8756eef
JasGH <81778219+JasGH@users.noreply.github.com>
2021-08-24 17:58:50 +0430

    test successful

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <video-player :options="videoOptions"/>
   </div>
 </template>
 
+<script>
+import VideoPlayer from "@/components/VideoPlayer.vue";
+
+export default {
+  name: "VideoExample",
+  components: {
+    VideoPlayer
+  },
+  data() {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        controlBar: {
+          // currentTimeDisplay: true,
+          TimeDivider: true,
+          children: [
+            'playToggle',
+            'CurrentTimeDisplay',
+            'progressControl',
+            'TimeDivider',
+            'RemainingTimeDisplay',
+            'volumePanel',
+            'PlaybackRateMenuButton',
+            'SubtitlesButton',
+            'qualitySelector',
+            'PictureInPictureToggle',
+            'fullscreenToggle',
+          ],
+        },
+        playbackRates: [0.5, 0.75, 1, 1.25 , 1.5, 1.75 , 2 ,4],
+        sources: [
+          {
+            src:
+                "https://cdn.alaatv.com/media/1042/HD_720p/1042003xxxx.mp4",
+            type: 'video/mp4',
+            label: '720p',
+            selected: true,
+          },
+          {
+            src:
+                "https://cdn.alaatv.com/media/1042/hq/1042003xxxx.mp4",
+            type: 'video/mp4',
+            label: '480P',
+            selected: true,
+          },
+          {
+            src:
+                "https://cdn.alaatv.com/media/1042/240p/1042003xxxx.mp4",
+            type: 'video/mp4',
+            label: '240p',
+            selected: true,
+          }
+        ]
+      }
+    }
+  }
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+.video-js .vjs-current-time { display: block; }
 </style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
+</template>
+
+<script>
+import videojs from 'video.js';
+require("video.js/dist/video-js.css");
+require('@silvermine/videojs-quality-selector')(videojs);
+require('@silvermine/videojs-quality-selector/dist/css/quality-selector.css')
+export default {
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      player: null
+    }
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    })
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+}
+</script>
